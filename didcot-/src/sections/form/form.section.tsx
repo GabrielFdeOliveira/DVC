a384f582798604817b857a06b135385031416e33
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2023-06-15 15:53:43 +0100

    Submit form created

--- didcot-/src/sections/form/form.section.tsx
+++ didcot-/src/sections/form/form.section.tsx
@@ -1,10 +1,50 @@
-import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Typography, Button, TextField, List, ListItem, ListItemText } from '@mui/material';
 
 export const FormSection: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [nameList, setNameList] = useState<string[]>([]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Regular expression to check for letters and spaces only
+    const validInput = /^[a-zA-Z\s]*$/.test(input);
+
+    const validLength = input.length <= 30;
+
+    if (validInput && validLength) {
+      setNameList((prevListItems) => [...prevListItems, input]);
+      setInput('');
+    } else {
+      // Display error or show validation message
+      if (!validInput) {
+        console.log('Input should contain only letters and spaces.');
+      }
+      if (!validLength) {
+        console.log('Input length should be less than or equal to 30 characters.');
+      }
+    }
+  };
+  
   return (
     <Box py={4}>
       <Typography variant="h5" mb={3}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste praesentium sequi non rem placeat unde.
+        <form onSubmit={handleSubmit}>
+          <TextField
+          label="Enter your name"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          />
+          <Button type='submit'>Submit</Button>
+        </form>
+        <List>
+          {nameList.map((item) => (
+            <ListItem key={item}>
+              <ListItemText primary={item} />
+            </ListItem>
+          ))}
+        </List>
       </Typography>
     </Box>
   );
